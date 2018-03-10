d840a24530da37874528e2f072c112f5372715a7
Daniel Abegglen | IQantum <daniel.abegglen@iqantum.ch>
2018-03-10 17:25:32 +0100

    Revert back

--- drivers/NQ9021/device.js
+++ drivers/NQ9021/device.js
@@ -15,64 +15,7 @@ class PowerMeter_NQ9021 extends ZwaveDevice {
       this.registerCapability('meter_power', 'METER', {
           getOpts: {
               getOnOnline: true,
-              pollInterval: 840000            
-          },
-          getParserV2: () => ({
-              Properties1: {
-                  Scale: 0,
-              },
-          }),
-          getParserV4: () => ({
-              Properties1: {
-                  'Rate Type': 'Import',
-                  Scale: 0,
-              },
-              'Scale 2': 0,
-          }),
-          report: 'BASIC_REPORT',
-          reportParserV1: report => {
-              if (report &&
-                  report.hasOwnProperty('Meter Type') &&
-                  (report['Meter Type'] === 'Electric meter' || report['Meter Type'] === 1) &&
-                  report.hasOwnProperty('Properties1') &&
-                  report.Properties1.hasOwnProperty('Scale') &&
-                  report.Properties1.Scale === 0) {
-                  return report['Meter Value (Parsed)'];
-              }
-              return null;
-          },
-          reportParserV2: report => {
-              if (report &&
-                  report.hasOwnProperty('Properties1') &&
-                  report.Properties1.hasOwnProperty('Meter Type') &&
-                  (report.Properties1['Meter Type'] === 'Electric meter' || report.Properties1['Meter Type'] === 1) &&
-                  report.hasOwnProperty('Properties2') &&
-                  report.Properties2.hasOwnProperty('Scale') &&
-                  report.Properties2.Scale === 0) {
-                  return report['Meter Value (Parsed)'];
-              }
-              return null;
-          },
-          reportParserV3: report => {
-              if (report &&
-                  report.hasOwnProperty('Properties1') &&
-                  report.Properties1.hasOwnProperty('Meter Type') &&
-                  (report.Properties1['Meter Type'] === 'Electric meter' || report.Properties1['Meter Type'] === 1) &&
-                  report.Properties1.hasOwnProperty('Scale bit 2') &&
-                  report.Properties1['Scale bit 2'] === false &&
-                  report.hasOwnProperty('Properties2') &&
-                  report.Properties2.hasOwnProperty('Scale bits 10') &&
-                  report.Properties2['Scale bits 10'] === 0) {
-                  return report['Meter Value (Parsed)'];
-              }
-              return null;
-          },
-
-      });
-
-      // register a report listener
-      this.registerReportListener('BASIC', 'BASIC_REPORT', (rawReport, parsedReport) => {
-          console.log('registerReportListener', rawReport, parsedReport);
+          }
       });
 	
 	// define FlowCardAction to reset meter_power
